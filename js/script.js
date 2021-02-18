d2db605607f3c07e9e8c9d22619ec84d77499ad2
webstormAlex <79262499+webstormAlex@users.noreply.github.com>
2021-02-18 22:03:54 +0300

    one

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+  let hamburger = document.querySelector('#hamburger');
+  hamburger.onclick = function () {
+    hamburger.classList.toggle('hamburger_active');
+  }
+}
+openMenu();
